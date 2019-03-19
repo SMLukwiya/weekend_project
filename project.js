c178054c01e4acdfdf94e6d5e7ea5765386b7aa1
SMLukwiya <sundaymorganl@gmail.com>
2019-03-19 19:57:04 +0300

    this may just be the last improvement am making

--- project.js
+++ project.js
@@ -48,67 +48,52 @@ const isOdd = (n)=> {//checks if number is odd
 
 const numberIdentifier = (n)=> {//checks through given numbers from 0 to maximum number while printing their corresponding identifiers
   var
-    case1,case1_dup=[],addedArray=[],counter
+    case1,case2=[]//variable case 2 is assigned outside the loop because we want it to collect all the elements that will be pushed
 
   for (num=0; num<=n; num++) {
     case1=[];//this variable is located inside the loop so that each time a value is pushed, the result will be printed.
     if (isFizzbuzz(num) && isEven(num)) {
       case1.push("even","fizzBuzz");//pushes the identifiers to an empty array called case1 located inside the for loop if the condition is passed
-      case1_dup.push("even","fizzBuzz");
-      /*//pushed the identifiers to a different empty array(case1_dup), located outside the loop, this will make it easy
-       to count the occurrences of the identifiers
-      */
     }
     else if (isFizzbuzz(num) && isOdd(num)) {
       case1.push("odd","fizzBuzz");
-      case1_dup.push("odd","fizzBuzz");
     }
     else if (isBuzz(num) && isOdd(num) && isPrime(num)) {
       case1.push("odd","prime","buzz");
-      case1_dup.push("odd","prime","buzz");
     }
     else if (isFizz(num) && isOdd(num) && isPrime(num)){
       case1.push("odd","prime","fizz");
-      case1_dup.push("odd","prime","fizz");
     }
     else if (isBuzz(num) && isOdd(num)) {
       case1.push("odd","buzz");
-      case1_dup.push("odd","buzz");
     }
     else if (isFizz(num) && isOdd(num)) {
       case1.push("odd","fizz");
-      case1_dup.push("odd","fizz");
     }
     else if (isBuzz(num) && isEven(num)) {
       case1.push("even","buzz");
-      case1_dup.push("even","buzz");
     }
     else if (isFizz(num) && isEven(num)) {
       case1.push("even","fizz");
-      case1_dup.push("even","fizz");
     }
     else if (isOdd(num) && isPrime(num)) {
       case1.push("odd","prime");
-      case1_dup.push("odd","prime");
     }
     else if (isEven(num) && isPrime(num)) {
       case1.push("even","prime");
-      case1_dup.push("even","prime");
     }
     else if (isOdd(num)) {
       case1.push("odd")
-      case1_dup.push("odd");
     }
     else {
       case1.push("even")
-      case1_dup.push("even");
     }
     console.log(num,case1);//prints the num alongside its identifiers starting from 0
+    case2.push(case1)//the values from case1 is continously pushed to the array case2 after every iteration
   }
 
-  addedArray=case1_dup;
-  return addedArray;
-  /*the array located outside the loop returns the overall array after pushing all the
+  return case2;
+  /*the array case2 located outside the loop returns all the
   identifiers, this will be used for ounting the occurrences of the identifiers
   */
 }
@@ -122,11 +107,13 @@ const counter =(array) => {//counts the occurrences of the identifiers
     /*iterates through an array starting from element at first position and keeps count of
     the number of occurrences of different identifiers until everything in the array has been counted
     */
-    if (count[array[i]]) {
-    count[array[i]] += 1;
-    } else {
-    count[array[i]] = 1;
-   }
+    for( var j=0;j<array[i].length; j++) {
+      if (count[array[i][j]]) {
+      count[array[i][j]] += 1;
+      } else {
+      count[array[i][j]] = 1;
+      }
+    }
 }
 return count;//returns the number of occurrences of each element
 
